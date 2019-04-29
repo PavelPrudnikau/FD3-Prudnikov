99ef03109bf0c080c55b01711f8da6f219e80cdf
Pavel Prudnikov <pavel.prudnikov@mail.ru>
2019-04-29 12:30:00 +0300

    ProductInfo.js reworked

--- ishop3/components/ProductInfo.js
+++ ishop3/components/ProductInfo.js
@@ -20,7 +20,7 @@ class ProductInfo extends React.Component {
         url:         this.props.product.url,
         quantity:    this.props.product.quantity,
         product:     this.props.product,
-        saveBlocked: true,
+        saveBlocked: false,
         errName:     "",
         errPrice:    "",
         errUrl:      "",
@@ -44,9 +44,9 @@ class ProductInfo extends React.Component {
         let tempProduct = {
             ...this.state.product, name: this.state.name, price: parseInt(this.state.price), url: this.state.url, quantity: parseInt(this.state.quantity)
         };
-        this.checkData();
+        this.validData();
         
-        if(!this.state.saveBlocked)
+        if(!this.validData())
         {
             this.props.cbDataSave(tempProduct);
             this.props.cbBlockTable(false);
@@ -58,10 +58,10 @@ class ProductInfo extends React.Component {
         this.props.cbBlockTable(false);
     }
 
-    checkData = (EO) => {
+    validData = (EO) => {
         
         var isError = false;
-        //if (EO.target.name == "InfoName") {
+
             if(typeof this.state.name != "string" || this.state.name == "")
             {
                 this.setState( {errName: "Value must be a string"} );
@@ -69,8 +69,7 @@ class ProductInfo extends React.Component {
             }
             else 
                 this.setState( {errName: ""} );
-        //}
-       // else if (EO.target.name == "InfoPrice") {
+            
             if(isNaN(this.state.price) || this.state.price < 0 || this.state.price == "")
             {
                 this.setState( {errPrice: "Value must be a number greater than 0"} );
@@ -78,17 +77,15 @@ class ProductInfo extends React.Component {
             }
             else
                 this.setState( {errPrice: ""} );
-        //}
-       // else if (EO.target.name == "InfoUrl") {
+            
             if(this.state.url.indexOf('http://') < 0 || this.state.url == "")
             {
                 this.setState( {errUrl: "Value shoud have 'http://' "} );
                 isError = true;
             }
-                else 
+            else 
                 this.setState( {errUrl: ""} );
-       // }
-        //else if (EO.target.name == "InfoQuantity") {
+            
             if(isNaN(this.state.quantity) || this.state.quantity < 0 || this.state.quantity == "")
             {
                 this.setState( {errQuantity: "Value must be a positive integer"} );
@@ -96,9 +93,11 @@ class ProductInfo extends React.Component {
             }
             else 
                 this.setState( {errQuantity: ""} );
-        //}
+        
         this.setState( {saveBlocked: isError} );
-
+        //this.setState( (prevState, props) => { return {saveBlocked:isError}; } );
+        
+        return isError;
     }
 
     productChanged = (EO) => {
@@ -134,32 +133,32 @@ class ProductInfo extends React.Component {
                     <br/>
                     <label>
                         <span className='inputName'>Name: </span>
-                        {this.props.workMode==1 && <input type='text' name='InfoName' className='' onChange={this.productChanged} onBlur={this.checkData}/>}
-                        {this.props.workMode==2 && <input type='text' name='InfoName' className='' onChange={this.productChanged} onBlur={this.checkData} value={this.state.name}/>}
+                        {this.props.workMode==1 && <input type='text' name='InfoName' className='' onChange={this.productChanged} onBlur={this.validData}/>}
+                        {this.props.workMode==2 && <input type='text' name='InfoName' className='' onChange={this.productChanged} onBlur={this.validData} value={this.state.name}/>}
                         {this.props.workMode==3 && this.state.name }
                         <span className='error'>{this.state.errName}</span>
                     </label>
                     <br/>
                     <label>
                         <span className='inputName'>Price: </span>
-                        {this.props.workMode==1 && <input type='text' name='InfoPrice' className='' onChange={this.productChanged} onBlur={this.checkData}/>}
-                        {this.props.workMode==2 && <input type='text' name='InfoPrice' className='' onChange={this.productChanged} onBlur={this.checkData} value={this.state.price}/>}
+                        {this.props.workMode==1 && <input type='text' name='InfoPrice' className='' onChange={this.productChanged} onBlur={this.validData}/>}
+                        {this.props.workMode==2 && <input type='text' name='InfoPrice' className='' onChange={this.productChanged} onBlur={this.validData} value={this.state.price}/>}
                         {this.props.workMode==3 && this.state.price }
                         <span className='error'>{this.state.errPrice}</span>
                     </label>
                     <br/>
                     <label>
                         <span className='inputName'>Url: </span>
-                        {this.props.workMode==1 && <input type='text' name='InfoUrl' className='' onChange={this.productChanged} onBlur={this.checkData}/>}
-                        {this.props.workMode==2 && <input type='text' name='InfoUrl' className='' onChange={this.productChanged} onBlur={this.checkData} value={this.state.url}/>}
+                        {this.props.workMode==1 && <input type='text' name='InfoUrl' className='' onChange={this.productChanged} onBlur={this.validData}/>}
+                        {this.props.workMode==2 && <input type='text' name='InfoUrl' className='' onChange={this.productChanged} onBlur={this.validData} value={this.state.url}/>}
                         {this.props.workMode==3 && this.state.url }
                         <span className='error'>{this.state.errUrl}</span>
                     </label>
                     <br/>
                     <label>
                         <span className='inputName'>Quantity: </span>
-                        {this.props.workMode==1 && <input type='text' name='InfoQuantity' className='' onChange={this.productChanged} onBlur={this.checkData}/>}
-                        {this.props.workMode==2 && <input type='text' name='InfoQuantity' className='' onChange={this.productChanged} onBlur={this.checkData} value={this.state.quantity}/>}
+                        {this.props.workMode==1 && <input type='text' name='InfoQuantity' className='' onChange={this.productChanged} onBlur={this.validData}/>}
+                        {this.props.workMode==2 && <input type='text' name='InfoQuantity' className='' onChange={this.productChanged} onBlur={this.validData} value={this.state.quantity}/>}
                         {this.props.workMode==3 && this.state.quantity }
                         <span className='error'>{this.state.errQuantity}</span>
                     </label>
