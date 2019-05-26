bf629cec3c0aac663d74a109e6c4d94530c89ad0
Pavel Prudnikov <pavel.prudnikov@mail.ru>
2019-05-26 18:53:04 +0300

    mobile

---
+++ Mobile/App.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MobileCompany from './components/MobileCompany';
+
+// если необходимо, вид сборки можно проверить в коде:
+// if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+
+let companyName='Velcom';
+let clientsArr=[ 
+  {id:1, fam:"Иванов",    im:"Иван",      otch:"Иванович",    balance:200}, 
+  {id:2, fam:"Сидоров",   im:"Сидор",     otch:"Сидорович",   balance:250}, 
+  {id:3, fam:"Петров",    im:"Пётр",      otch:"Петрович",    balance:180},
+  {id:4, fam:"Григорьев", im:"Григорий",  otch:"Григорьевич", balance:-220},
+];
+
+ReactDOM.render(
+  <MobileCompany 
+    name={companyName}
+    clients={clientsArr}
+  />
+  , document.getElementById('container') 
+);
+
